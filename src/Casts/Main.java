fc604e363175c3a7fd42c085387ef2d215bb929e
Alexander Craig <alexandercraig@ORRHS.ORG>
2020-10-08 13:21:00 -0400

    added structure

---
+++ src/Casts/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println(1 / 3);
+        System.out.println(1.0 / 3);
+        System.out.println(1 / 3.0);
+        System.out.println((double) 1 / 3);
+    }
+    
+}
+
---
+++ src/Expressions and Assignment/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println(5+2);
+
+        int x = 5;
+        int y = 3;
+
+        y = 5;
+
+        System.out.println(x+y);
+
+        System.out.println(5/2);
+
+    }
+    
+}
